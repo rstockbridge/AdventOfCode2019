d66cc32b8adfc624350bd9c163ff0d7e888fc92f
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2019-12-01 13:52:49 -0500

    Add utilities

--- day10/build.gradle.kts
+++ day10/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(project(":utilities"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
